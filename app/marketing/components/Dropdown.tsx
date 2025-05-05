3c16883e72e53f557b92cf60efb11429c96bb43f
ClinkThearly <clinkthearly@gmail.com>
2025-05-05 16:09:50 -0400

    fix(marketing): add Dropdown shim to satisfy SentenceForm import

---
+++ app/marketing/components/Dropdown.tsx
@@ -0,0 +1,7 @@
+// app/marketing/components/Dropdown.tsx
+// ---------------------------------------------------
+// Tiny shim so legacy code `import Dropdown from './Dropdown'`
+// continues to work.  It simply re-exports your existing
+// CustomDropdown component.
+
+export { default } from './CustomDropdown';
